045bbc4e504bdff27983fff7b465d988f23c87b9
Asura0107 <mircoorosio@gmail.com>
2023-11-07 16:44:50 +0100

    U2-W3-D2-storage

--- timer.js
+++ timer.js
@@ -3,13 +3,13 @@ let minutes = 0;
 
 const count = function () {
   const span = document.getElementById("timespace");
-  span.innerText = minutes + " : " + seconds;
 
   seconds++;
   if (seconds >= 60) {
     minutes++;
     seconds = 0;
   }
+  span.innerText = minutes + " : " + seconds;
 };
 const timer = setInterval(count, 1000);
 window.addEventListener("DOMContentLoaded", () => {
